e4ec0d14e764918bfe5741dfe8240678ad367a3c
itsmarkodowd <16547497+itsmarkodowd@users.noreply.github.com>
2021-03-14 21:47:59 +0000

    Updates

---
+++ precache-manifest.a4ee95bc7133b680b459d8e781ed393e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5baf892cc6e48b97684c8a3a5a2fa916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84d58240539c34058d4e",
+    "url": "/static/css/16.d48510fc.chunk.css"
+  },
+  {
+    "revision": "ef830a16fd52bf68b96c",
+    "url": "/static/css/main.667846d7.chunk.css"
+  },
+  {
+    "revision": "a3f7dc1a85cc8848d6ff",
+    "url": "/static/js/0.e7660ec9.chunk.js"
+  },
+  {
+    "revision": "84d58240539c34058d4e",
+    "url": "/static/js/16.3550880b.chunk.js"
+  },
+  {
+    "revision": "eb56b19eba63a53416f4fd778023529f",
+    "url": "/static/js/16.3550880b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecbc5104017768a69fa",
+    "url": "/static/js/Accessibility.52b56e74.chunk.js"
+  },
+  {
+    "revision": "bdea5b9a86c0ca4ddad6",
+    "url": "/static/js/Chords.a51a7125.chunk.js"
+  },
+  {
+    "revision": "81ce22a770cbe0561173",
+    "url": "/static/js/ClefToTab.662a19fa.chunk.js"
+  },
+  {
+    "revision": "55d1038ae596185a3279",
+    "url": "/static/js/Community.7a5c8a34.chunk.js"
+  },
+  {
+    "revision": "4010db5c46b0bcf4026f",
+    "url": "/static/js/Homepage.38415a34.chunk.js"
+  },
+  {
+    "revision": "ec8e5a72942e6559a717",
+    "url": "/static/js/HowToReadTab.090e927d.chunk.js"
+  },
+  {
+    "revision": "6f9268ee431ac10e2f84",
+    "url": "/static/js/HowToString.fbbb0270.chunk.js"
+  },
+  {
+    "revision": "e57b5df5ebdb7e7c92e7",
+    "url": "/static/js/HowToTune.e3a15e90.chunk.js"
+  },
+  {
+    "revision": "4a8a0bce38d2d7a89241",
+    "url": "/static/js/Lessons.b248a521.chunk.js"
+  },
+  {
+    "revision": "bbd49759570608e9f600",
+    "url": "/static/js/Newsletter.e0611e81.chunk.js"
+  },
+  {
+    "revision": "0730758db0bc54ead0d1",
+    "url": "/static/js/NotFound.db93361f.chunk.js"
+  },
+  {
+    "revision": "2c8eb647840593fd7088",
+    "url": "/static/js/Tablature.c607cab4.chunk.js"
+  },
+  {
+    "revision": "c8a0133c13e32efbf918",
+    "url": "/static/js/Terminology.eaf1dab7.chunk.js"
+  },
+  {
+    "revision": "ef830a16fd52bf68b96c",
+    "url": "/static/js/main.f26bdfaa.chunk.js"
+  },
+  {
+    "revision": "9416e5e00fd75de17b3f",
+    "url": "/static/js/runtime-main.33173e6a.js"
+  },
+  {
+    "revision": "be1e6672908f2d76091de9ac60983c89",
+    "url": "/static/media/OpenDyslexic-Regular.be1e6672.otf"
+  },
+  {
+    "revision": "a45efc36ae729f40ce0152651545bf1d",
+    "url": "/static/media/meathflf-webfont.a45efc36.woff"
+  },
+  {
+    "revision": "e11567429f8c37aba4ba924b24acca8f",
+    "url": "/static/media/meathflf-webfont.e1156742.woff2"
+  },
+  {
+    "revision": "5fcb870189679bd073b7974bd4a4a8e1",
+    "url": "/static/media/session.5fcb8701.jpg"
+  }
+]);
